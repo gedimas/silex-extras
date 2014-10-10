e09cd80b90b0750cf6870ca930e4a9ccbeb17da4
gedimas <jeefo4@gmail.com>
2014-10-10 13:19:27 +0300

    Update Ftp.php
    Add support for passive mode to Ftp

--- src/Aptoma/Ftp/Ftp.php
+++ src/Aptoma/Ftp/Ftp.php
@@ -199,14 +199,28 @@ class Ftp
 
     /**
      * @return resource
+     * @throws Exception\FtpException
      */
     private function getConnection()
     {
         if (!$this->connection) {
             $this->connection = ftp_connect($this->hostname);
+            if (!$this->connection) {
+                throw new FtpException(sprintf('Error connecting to FTP server at %s.', $this->hostname));
+            }
+
             ftp_login($this->connection, $this->username, $this->password);
         }
 
         return $this->connection;
     }
+
+	/**
+	 * @param bool $state
+	 * @return bool
+	 */
+	public function setPassiveMode($state)
+	{
+		return ftp_pasv($this->getConnection(), $state);
+	}
 }
